a1c7a36dd4ff41578d367877a48bc64048e4b5b2
Andrew Morton <andrewmorton2@hotmail.co.uk>
2019-11-09 19:23:44 +0100

    Added global navbar + registration page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Homepage from "./Homepage";
+import Register from "./Register";
+import Login from "./Login";
+
+import Nav from "react-bootstrap/Nav";
+import NavBar from "react-bootstrap/Navbar";
 
 function App() {
   return (
+    <div>
+    <NavBar bg="light" expand="lg">
+      <NavBar.Collapse id="basic-navbar-nav" />
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+      </Nav>
+    </NavBar>
+
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
           <Homepage />
         </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
       </Switch>
     </BrowserRouter>
+    </div>
   );
 }
 
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,4 +1,7 @@
+// React imports
 import React from "react";
+
+// Bootstrap imports
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,6 +9,9 @@ import Carousel from 'react-bootstrap/Carousel';
 import Background from "./img/city-skyline.jpg";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+
+// React Router Bootstrap import
+import { LinkContainer } from 'react-router-bootstrap';
 
 import "./Homepage.css"
 
@@ -19,8 +25,10 @@ function Homepage() {
           <Carousel.Caption>
             <h1>Let us do the work for you.</h1>
             <br/>
-            <Button size="xl" variant="info">Let's go</Button>
-            <Button size="xl" variant="info">Let's go</Button>
+              <LinkContainer to="/register">
+                <Button className="mx-4" size="xl" variant="info">Get Started</Button>
+              </LinkContainer>
+              <Button className="mx-4" size="xl" variant="info">Login</Button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -29,7 +37,7 @@ function Homepage() {
 
       <Container>
         <Col>
-          <h1>Welcome to your gap year made easy.</h1>
+          <h2>Welcome to your gap year made easy.</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <p></p>
         </Col>
